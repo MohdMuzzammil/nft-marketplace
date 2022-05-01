d50cda34e965e3ebb244a5d2335c01ccfca529b3
Mohammed Muzzammil <muzzammilmohammed32@gmail.com>
2022-05-02 00:05:17 +0530

    Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,18 @@
 /* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle")
+const fs = require("fs");
+const privateKey = fs.readFileSync("./.secert").toString(); 
+const projectId = fs.readFileSync("./.projectId").toString(); 
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    rinkbey: {
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
     },
 //  unused configuration commented out for now
 //  mumbai: {
